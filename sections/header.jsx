82957b400ad3045308a8856d0a34af4ad373d902
mikeyqq <acmhuang@gmail.com>
2022-12-01 04:35:21 -0500

    added dark mode button

---
+++ sections/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Darkmodebutton from "../components/darkModeButton";
+
+export default function Header() {
+  return (
+    <div>
+      <Darkmodebutton />
+    </div>
+  );
+}
