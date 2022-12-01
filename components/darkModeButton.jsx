3423e86e8f0da0a22daefdbe5823860ab7233ee8
mikeyqq <acmhuang@gmail.com>
2022-12-01 17:38:36 -0500

    completed toggle dark mode button/ added animation feature to the button

--- components/darkModeButton.jsx
+++ components/darkModeButton.jsx
@@ -1,18 +1,52 @@
-import React from "react";
 import { useTheme } from "next-themes";
+import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
+import { AnimatePresence, motion } from "framer-motion";
 
-export default function Darkmodebutton() {
-  const { theme, setTheme } = useTheme();
+const Header = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+
+  const renderThemeChanger = () => {
+    const currentTheme = theme === "system" ? systemTheme : theme;
+
+    if (currentTheme === "dark") {
+      return (
+        <div className="mx-5 my-5 p-2 bg-amber-400 rounded-lg hover:bg-amber-500">
+          <SunIcon
+            className="text-white w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7"
+            role="button"
+            onClick={() => setTheme("light")}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div className="mx-5 my-5 p-2 border-solid shadow-lg  bg-violet-700 rounded-lg">
+          <MoonIcon
+            className=" text-white w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7"
+            role="button"
+            onClick={() => setTheme("dark")}
+          />
+        </div>
+      );
+    }
+  };
+
   return (
-    <div>
-      <div className="bg-white dark:bg-black">
-        <button
-          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-          className="px-4 py-2 text-white bg-black rounded dark:bg-white dark:text-black"
-        >
-          Dark Toggle
-        </button>
-      </div>
-    </div>
+    <AnimatePresence exitBeforeEnter initial={false}>
+      <motion.div
+        style={{ display: "inline-block" }}
+        key={theme}
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 20, opacity: 0 }}
+        transition={{ duration: 0.2 }}
+      >
+        <div className="flex items-center  md:ml-80">
+          <div className="">{renderThemeChanger()}</div>
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
-}
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../sections/header";
 import Main from "../sections/main";
+import Footer from "../sections/footer";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 };
